f03ee4b906d1c66886ffe4ce1a3563f39fd16605
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-11-14 12:01:38 +0700

    Initial commit (Otw Create Account)

---
+++ app/src/main/java/ebusiness/guitarmaestro/activities/Login.java
@@ -0,0 +1,47 @@
+package ebusiness.guitarmaestro.activities;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ebusiness.guitarmaestro.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView textView = findViewById(R.id.signup_text);
+
+        SpannableString ss = new SpannableString("Don't have an account? Signup here.");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent i = new Intent(Login.this, CreateAccount.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.parseColor("#4BAAEA"));
+                ds.setUnderlineText(false);
+            }
+        };
+
+        ss.setSpan(clickableSpan, 30, 35, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
