31aa8ee73890231426b6419e89e5e3181501f8d6
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2019-11-16 17:46:05 +0700

    kurang: knob, bodies, guitarnecks, cart, paymnent
    mangat lan!

---
+++ app/src/main/java/ebusiness/guitarmaestro/activities/Bodies.java
@@ -0,0 +1,14 @@
+package ebusiness.guitarmaestro.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bodies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bodies);
+    }
+}
